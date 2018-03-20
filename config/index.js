39940e6c8869f6f36b9e07b5e9fad27bba801dca
yuki.hayakawa <yuki.hayakawa0823@gmail.com>
2018-03-21 03:30:36 +0900

    ファイル構成変更

--- config/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  name: 'Markdown memo',
-  defaultDir: 'markdown/',
-};
---
+++ config/index.ts
@@ -0,0 +1,6 @@
+const config = {
+  name: 'Markdown memo',
+  defaultDir: 'markdown/',
+  api: 'http://localhost:3030/',
+};
+export default config;
